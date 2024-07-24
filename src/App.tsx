2857fefc9cd636791a496120d2e9a8a0d301872e
georgi-kr <gkrastev9715@abv.bg>
2024-07-24 20:45:55 +0300

    refactor: vite with yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="./assets/react.svg" className="logo" alt="Vite logo" />
+        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="./vite.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -18,14 +20,12 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
-
-export default App
+  );
+};
